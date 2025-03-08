5e06bda33b6daa1a1f10cdeab3304e23a4681efe
Dibwarjeet Bordolai <71434149+git-dibwar@users.noreply.github.com>
2025-03-08 13:25:42 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://vfjvxgwugdgfntjekzux.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmanZ4Z3d1Z2RnZm50amVrenV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzNjQ2MzEsImV4cCI6MjA1Njk0MDYzMX0.bDgUyiud-Zdw8BFRsxIh4gV80zSykU8gBwq5gh9_JkM";
+const SUPABASE_URL = "https://hucnlvxnwazlqukzsvdb.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1Y25sdnhud2F6bHF1a3pzdmRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MTk4NjQsImV4cCI6MjA1Njk5NTg2NH0.TLVa7nRFMpbBxQsCRGtXwx-sUfKTzTLGUohyxhRp6KI";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
